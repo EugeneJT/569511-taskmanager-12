ba756daf04c35e73a12aace49664b6f7f93e9b80
Eugene Nasonov <eugenetikk@gmail.com>
2020-08-27 00:57:40 +0300

    added module5-task1

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,5 +1,5 @@
 import {COLORS, DESCRIPTIONS, MaxDate} from "../const.js";
-import {getRandomInteger} from "../utils.js";
+import {getRandomInteger} from "../utils/common.js";
 
 const generateDescription = () => {
 
