a6deed56d2b4ea8dd3531b8af4400433e9ec35e2
Eugene Nasonov <eugenetikk@gmail.com>
2020-09-24 04:11:39 +0300

    added module7-task1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import SiteMenuView from "./view/site-menu.js";
+import StatisticsView from "./view/statistics.js";
 import {generateTask} from "./mock/task.js";
 import BoardPresenter from "./presenter/board.js";
 import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/task.js";
 import FilterModel from "./model/filter.js";
-import {render, RenderPosition} from "./utils/render.js";
+import {render, RenderPosition, remove} from "./utils/render.js";
+import {MenuItem, UpdateType, FilterType} from "./const.js";
 
 const TASK_COUNT = 22;
 
@@ -17,16 +19,59 @@ const filterModel = new FilterModel();
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuView();
 
 
-render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
 
-filterPresenter.init();
-boardPresenter.init();
+const handleTaskNewFormClose = () => {
+  siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = false;
+  siteMenuComponent.setMenuItem(MenuItem.TASKS);
+};
 
-document.querySelector(`#control__new-task`).addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  boardPresenter.createTask();
-});
+let statisticsComponent = null;
+
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+      // Скрыть статистику
+      remove(statisticsComponent);
+
+      // Показать доску
+      boardPresenter.destroy();
+      filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
+      boardPresenter.init();
+      // Показать форму добавления новой задачи
+      // Убрать выделение с ADD NEW TASK после сохранения
+      boardPresenter.createTask(handleTaskNewFormClose);
+      siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
+      break;
+    case MenuItem.TASKS:
+      // Показать доску
+      boardPresenter.init();
+
+      // Скрыть статистику
+      remove(statisticsComponent);
+
+      break;
+    case MenuItem.STATISTICS:
+      // Скрыть доску
+      boardPresenter.destroy();
+
+      // Показать статистику
+      statisticsComponent = new StatisticsView(tasksModel.getTasks());
+      render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
+      break;
+  }
+};
+
+
+siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
+filterPresenter.init();
+// Для удобства отладки скроем доску
+// boardPresenter.init();
+// и отобразим сразу статистику
+boardPresenter.init();
